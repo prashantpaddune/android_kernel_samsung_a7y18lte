ffb975fe62c16a9bdc6db8477236caef0320d94d
Diep Quynh <remilia.1505@gmail.com>
2020-04-20 22:04:01 +0700

    exynos-dm: Wrap exynos-ss debugging parts
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- drivers/soc/samsung/exynos-dm.c
+++ drivers/soc/samsung/exynos-dm.c
@@ -650,18 +650,22 @@ static bool max_flag = false;
 static int __policy_update_call_to_DM(enum exynos_dm_type dm_type, u32 min_freq, u32 max_freq)
 {
 	struct exynos_dm_data *dm;
+#ifdef CONFIG_EXYNOS_SNAPSHOT_DM
 	struct timeval pre, before, after;
+#endif
 #ifdef CONFIG_EXYNOS_ACPM
 	struct ipc_config config;
 	unsigned int cmd[4];
 	int size, ch_num, ret;
 #endif
+#ifdef CONFIG_EXYNOS_SNAPSHOT_DM
 	s32 time = 0, pre_time = 0;
 
 	exynos_ss_dm((int)dm_type, min_freq, max_freq, pre_time, time);
 
 	do_gettimeofday(&pre);
 	do_gettimeofday(&before);
+#endif
 
 	min_freq = min(min_freq, max_freq);
 
@@ -698,6 +702,7 @@ static int __policy_update_call_to_DM(enum exynos_dm_type dm_type, u32 min_freq,
 #endif
 
 out:
+#ifdef CONFIG_EXYNOS_SNAPSHOT_DM
 	do_gettimeofday(&after);
 
 	pre_time = (before.tv_sec - pre.tv_sec) * USEC_PER_SEC +
@@ -706,6 +711,7 @@ out:
 		(after.tv_usec - before.tv_usec);
 
 	exynos_ss_dm((int)dm_type, min_freq, max_freq, pre_time, time);
+#endif
 
 	return 0;
 }
@@ -766,6 +772,7 @@ static int __DM_CALL(enum exynos_dm_type dm_type, unsigned long *target_freq)
 	int ret;
 	unsigned int relation = EXYNOS_DM_RELATION_L;
 	u32 old_min_freq;
+#ifdef CONFIG_EXYNOS_SNAPSHOT_DM
 	struct timeval pre, before, after;
 	s32 time = 0, pre_time = 0;
 
@@ -773,6 +780,7 @@ static int __DM_CALL(enum exynos_dm_type dm_type, unsigned long *target_freq)
 
 	do_gettimeofday(&pre);
 	do_gettimeofday(&before);
+#endif
 
 	dm = &exynos_dm->dm_data[dm_type];
 	old_min_freq = dm->min_freq;
@@ -825,6 +833,7 @@ static int __DM_CALL(enum exynos_dm_type dm_type, unsigned long *target_freq)
 		max_order[i] = DM_EMPTY;
 	}
 
+#ifdef CONFIG_EXYNOS_SNAPSHOT_DM
 	do_gettimeofday(&after);
 
 	pre_time = (before.tv_sec - pre.tv_sec) * USEC_PER_SEC +
@@ -833,6 +842,7 @@ static int __DM_CALL(enum exynos_dm_type dm_type, unsigned long *target_freq)
 		(after.tv_usec - before.tv_usec);
 
 	exynos_ss_dm((int)dm_type, *target_freq, 3, pre_time, time);
+#endif
 
 	return 0;
 }
